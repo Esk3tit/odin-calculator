6cae2d662afb778adb5d1009729467cf09047f04
Khai Phan <khaiphn41@gmail.com>
2024-01-14 17:09:12 -0800

    Rewrite code for chaining and = w/ operator
    Had to rethink it through and refactor into functions to get less messy
    code. Didn't need two completely separate cases for operator button
    when getting first operator and subsequent ones for chaining
    just eval since the results of the eval will be used correctly for
    chaining when you run it through the initial code for no chaining
    and getting only first operator.
    
    Not replicating windows calc 1 to 1 since its too cancer lol

--- script.js
+++ script.js
@@ -28,9 +28,31 @@ function operate(operator, operand1, operand2) {
     }
 }
 
+function clear() {
+    displayResult.textContent = '0';
+    displayHistory.textContent = '';
+    operand1 = null;
+    operand2 = null;
+    operator = null;
+}
+
+function evaluate() {
+    if (shouldClearDisplay) return;
+    if (!operator) {
+        displayHistory.textContent = `${displayResult.textContent} =`;
+    } else {
+        operand2 = Number(displayResult.textContent);
+        displayHistory.textContent += ` ${operand2} =`;
+        displayResult.textContent = operate(operator, operand1, operand2);
+    }
+    shouldClearDisplay = true;
+    operator = null;
+}
+
 let operand1 = null;
 let operand2 = null;
 let operator = null;
+let shouldClearDisplay = false;
 
 // Hook up listener to all buttons with event delegation on the div with class "buttons"
 const buttons = document.querySelector('.buttons');
@@ -39,44 +61,20 @@ const displayHistory = document.querySelector('#display-history');
 
 buttons.addEventListener('click', function(e) {
     if (e.target.classList.contains('number')) {
-        // If the button clicked is a number, add it to the display
-        // Check if we need to clear the display first if it is the first number that we entered
-        // it should be the first number if the display is 0 and there is no display history/previous result
-        // We also want to clear the display if the previous button clicked was an operator so we can enter the second operand
-        if ((displayResult.textContent === '0' && displayHistory.textContent === '') || (operand1 && displayHistory.textContent !== '')) {
+        if ((displayResult.textContent === '0' && displayHistory.textContent === '') || shouldClearDisplay) {
             displayResult.textContent = '';
+            shouldClearDisplay = false;
         }
         displayResult.textContent += e.target.textContent;
     } else if (e.target.classList.contains('operator')) {
-        // If the button clicked is an operator, store the first operand and the operator
-        // else it might be part of a chain of operations, so we need to operate on the previous result, check if there is a previous result
-        if (displayHistory.textContent !== '') {
-            // If there is a previous result, then this must be a chain of operations so we have the second operand in the display
-            // and the operand1 is the previous result with the operator so we can operate on them and then update the display history
-            operand2 = Number(displayResult.textContent);
-            const result = operate(operator, operand1, operand2);
-            displayResult.textContent = result;
-            operand1 = result;
-            operator = e.target.textContent;
-        } else {
-            operand1 = Number(displayResult.textContent);
-            operator = e.target.textContent;
-        }
-        // Update the display history with the first operand and the operator
+        if (operator) evaluate();
+        operand1 = Number(displayResult.textContent);
+        operator = e.target.textContent;
         displayHistory.textContent = `${operand1} ${operator}`;
+        shouldClearDisplay = true;
     } else if (e.target.id === 'equals') {
-        // If the button clicked is the equals button, store the second operand and operate
-        // also update history with the second operand and equals sign
-        operand2 = Number(displayResult.textContent);
-        displayResult.textContent = operate(operator, operand1, operand2);
-        displayHistory.textContent += ` ${operand2} =`;
+        evaluate();
     } else if (e.target.id === 'clear') {
-        // If the button clicked is the clear button, clear the display
-        // and reset all state variables
-        displayResult.textContent = '0';
-        displayHistory.textContent = '';
-        operand1 = null;
-        operand2 = null;
-        operator = null;
+        clear();
     }
 });
